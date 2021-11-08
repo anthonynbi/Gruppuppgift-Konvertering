439332852f241a01ad84a47db440ba91ff4f700a
Jonathan <uddha@live.se>
2021-11-08 11:36:48 +0100

    Add project files.

---
+++ Köksmått/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Köksmått
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
