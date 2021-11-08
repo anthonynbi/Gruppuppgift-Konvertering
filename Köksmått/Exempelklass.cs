357808dee34aa08c135041b949de9eb61cee4c63
Jonathan <uddha@live.se>
2021-11-08 14:02:44 +0100

    Uppstäd

--- Köksmått/Exempelklass.cs
+++ Köksmått/Exempelklass.cs
@@ -8,22 +8,6 @@ namespace Köksmått
 {
     class Exempelklass
     {
-
-        public static Dictionary<string, double> measureList = new Dictionary<string, double>() { { "liter", 10 }, { "deciliter", 1 }, { "ukgallon", 45 }, { "ukpint", 5.7 } };
-
-
-
-        public static double litertogallon(double amount)
-        {
-            double sum = measureList["liter"] * amount / measureList["ukgallon"];
-            
-            return sum;
-        }
-
-        public static double litertopint(double amount)
-        {
-            double sum = measureList["liter"] * amount / measureList["ukpint"];
-            return sum;
-        }
+               
     }
 }
--- Köksmått/Program.cs
+++ Köksmått/Program.cs
@@ -6,23 +6,7 @@ namespace Köksmått
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hej Sebastian");
-
-            Console.WriteLine("Heipa Jonte");
-
-            Console.WriteLine("Vad gör du?");
-            Console.WriteLine("Funkar det här?");
-            Console.WriteLine("Testar på Mac...");
-            Console.WriteLine("hi boys");
-
-            Console.WriteLine("Testar en tredje gång eller nåt....");
-
-
-            
-            Console.WriteLine(Exempelklass.litertogallon(5));
-            Console.WriteLine(Exempelklass.litertopint(5));
-
-            Console.WriteLine("hi boys");
+         
         }
     }
 }
